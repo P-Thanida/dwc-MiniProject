2325a82b73e75d0d42f3b50998a2a1c322420511
P-Thanida <s6135512015@phuket.psu.ac.th>
2021-04-21 01:52:37 -0700

    Edit header

--- frontend/components/header.js
+++ frontend/components/header.js
@@ -1,13 +1,18 @@
+
+
 const headerStyle = {
-    backgroundColor: "red",
-    color: "white",
-    width: "100%",
-    height: "50px"
-  };
-  
-  const Header = () => (
-    <div className="Header" style={headerStyle}>
-        </div>
-  );
-  
-  export default Header;
+  backgroundColor: "red",
+  color: "white",
+  width: "100%",
+  height: "50px",
+  fontsize: "1000px"
+};
+
+const Header = () => (
+  <div > <br></br>
+    <div  className="Header" style={headerStyle}>Next-Movie</div>    
+  </div>
+);
+export default Header;
+
+
