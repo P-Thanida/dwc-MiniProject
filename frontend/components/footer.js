3c73f23529ffd6a40a36e9d637e597c63c28470b
Thanida-pai <s6135512015@phuket.psu.ac.th>
2021-04-23 02:17:32 +0700

    Add Footer

---
+++ frontend/components/footer.js
@@ -0,0 +1,5 @@
+import styles from '../styles/Home.module.css'
+
+export default function Footer() {
+    return <div className={styles.footer}><footer><p>©2021 | THANIDA</p></footer></div>
+}
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,39 +1,47 @@
 import Head from 'next/head'
 import Layout from '../components/layout'
-import Navbar from '../components/navbar'
+import Footer from '../components/footer'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import { motion } from "framer-motion";
 
 export default function Home({ token }) {
-
   return (
     <Layout>
       <Head>
         <title>Home Page</title>S
-      </Head>     
-      <div className={styles.header}><div></div>Next-Movie</div>  
- <Navbar />
-      <div className={styles.container}> <br></br>
-        
-        <h1>New Movie</h1>
-        <div className={styles['movie-container']}>
-          {["stranger", "lord", "deadpool", "yesday", "fantastic", "enola", "tenet", "raya", "maleficent", "bumblebee", "abominable", "guardians"].map((movie) => (
-            <Link href={movie}>
-              <a>
-                <motion.img
-                  layoutId={movie}
-                  className={styles.image}
-                  src={movie + ".jpg"}
-                  animate={{ scale: 1 }}
-                  whileHover={{ scale: 1.1 }}
-                />
-              </a>
-            </Link>
-          ))}
+      </Head>
+      
+      <div className={styles.row}>
+        <div className={styles.header}><p>Next-Movies</p></div>
+        <div className={styles.header2}>
+          <p>
+            <Link target="#" href="/login">login</Link>
+          </p>
         </div>
       </div>
-    </Layout>
+
+
+
+      <h1>New Movie</h1>
+      <div className={styles['movie-container']}>
+        {["stranger", "lord", "deadpool", "yesday", "fantastic", "enola", "tenet", "raya", "maleficent", "bumblebee", "abominable", "guardians"].map((movie) => (
+          <Link href={movie}>
+            <a>
+              <motion.img
+                layoutId={movie}
+                className={styles.image}
+                src={movie + ".jpg"}
+                animate={{ scale: 1 }}
+                whileHover={{ scale: 1.1 }}
+              />
+            </a>
+          </Link>
+        ))}
+      </div>
+      <Footer />
+
+    </Layout >
   )
 }
 
