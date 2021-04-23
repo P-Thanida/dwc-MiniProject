946d41bf910b90d550ab549eea4e716dd45f0de4
P-Thanida <s6135512015@phuket.psu.ac.th>
2021-04-24 03:32:41 +0700

    fix index layout

--- frontend/components/navbar.js
+++ frontend/components/navbar.js
@@ -1,21 +1,21 @@
-// import Link from 'next/link'
-// import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+import {
+    Button
+    , Card
+} from 'antd'
+const Navbar = () => (
+    <Card className={styles.nav}>
+        <div >
+            <div>Next-Movies</div>
+            <h1 >New Movie</h1>
 
-// const Navbar = () => (
-//     <div className={styles.nav}>
-//         <Link href="/"><a> Home </a></Link> |
-//         <Link href="/about"><a> About </a></Link> | 
-   
-//         {/* <Link href="/register"><a> Register </a></Link>  |
-//         <Link href="/login"><a> Login </a></Link> |
-//         <Link href="/profile"><a> Profile </a></Link> | 
-//         <Link href="/students"><a>Student</a></Link> |
-//         <Link href="/getConfig"><a> Config </a></Link> | 
-//         {/* <Link href="/foo"><a> Foo </a></Link> | */}
-//         {/* <Link href="/logout"><a> Logout </a></Link>  */} 
+            <div className={styles.userButtonContainer}>
+                <Button className={styles.userButton} type="primary">Register</Button>
+                <Button className={styles.userButton} type="primary">Login</Button>
+            </div>
+        </div>
+    </Card>
+)
 
-        
-//     </div>
-// )
-
-// export default Navbar
+export default Navbar
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -6,40 +6,50 @@ import Link from 'next/link'
 import { motion } from "framer-motion";
 import AccountImage from '../images/account.png'
 import Router from "next/router";
+import Navbar from '../components/navbar'
+import {
+  Button
+  , Card
+} from 'antd'
 
 export default function Home({ token }) {
   return (
     <Layout>
       <Head>
+        <link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/antd/2.9.3/antd.min.css' />
         <title>Home Page</title>S
       </Head>
 
-      <div className={styles.row}>
+      {/* <div className={styles.row}>
         <div className={styles.header}><p>Next-Movies</p></div>
         <div className={styles.header2}>
-        
+
           <div>  <Link href="/register"><a> Register </a></Link> <br></br>
             <img src={AccountImage} alt='account' id='AccountImage' />
           </div>
 
         </div>
-      </div>
-      <h1 className={styles.h1}>New Movie</h1>
-      <div className={styles['movie-container']}>
-        {["stranger", "lord", "deadpool", "yesday", "fantastic", "enola", "tenet", "raya", "maleficent", "bumblebee", "abominable", "guardians"].map((movie) => (
-          <Link href={movie}>
-            <a>
-              <motion.img
-                layoutId={movie}
-                className={styles.image}
-                src={movie + ".jpg"}
-                animate={{ scale: 1 }}
-                whileHover={{ scale: 1.1 }}
-              />
-            </a>
-          </Link>
-        ))}
-      </div>
+      </div> */}
+      {/*  */}
+
+      <Navbar />
+      <Card>
+        <div className={styles['movie-container']}>
+          {["stranger", "lord", "deadpool", "yesday", "fantastic", "enola", "tenet", "raya", "maleficent", "bumblebee", "abominable", "guardians"].map((movie) => (
+            <Link href={movie}>
+              <a>
+                <motion.img
+                  layoutId={movie}
+                  className={styles.image}
+                  src={movie + ".jpg"}
+                  animate={{ scale: 1 }}
+                  whileHover={{ scale: 1.1 }}
+                />
+              </a>
+            </Link>
+          ))}
+        </div>
+      </Card>
       <Footer />
 
     </Layout >
